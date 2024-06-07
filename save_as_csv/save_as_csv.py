bba25e9ae913d8ae7c1f5ecabb5c0f78b1783f59
Index <nsxz1359@naver.com>
2024-06-07 23:55:38 +0900

    Update save_as_csv.py

--- save_as_csv/save_as_csv.py
+++ save_as_csv/save_as_csv.py
@@ -1,5 +1,3 @@
-import csv
-
 def split_file_by_commas(filename):
 	with open(filename, 'r', encoding='utf-8') as file:
 		lines = file.readlines()
@@ -28,27 +26,17 @@ def modify_first_element(blocks):
 			block[0] = ','.join(first_element)
 
 # 파일 경로 지정
-input_filename = 'input.csv'  # 처음에 xlsx를 csv로 변환시킨 파일
-output_filename = 'output.csv'  # 단순 파일 입출력으로 저장시킬 파일
-output2_filename = 'res.csv'  # csv 파일 입출력으로 csv로 저장시킬 파일
+input_filename = 'input.csv'
+output_filename = 'output.csv'
 
 # 파일을 읽고 분리된 블록을 가져오기
 blocks = split_file_by_commas(input_filename)
 # blocks 리스트에 대해 수정 수행
 modify_first_element(blocks)
-#print(blocks[960])
 
 with open(output_filename, 'w', newline='', encoding='utf-8') as f:
 	for i in range(len(blocks)):
 		for block in blocks[i]:
 			f.write(block + '\n')
-
-# 기존 파일을 읽어와서 동일한 형식으로 다시 저장
-with open(output_filename, 'r', newline='', encoding='utf-8-sig') as infile, \
-  open(output2_filename, 'w', newline='', encoding='utf-8-sig') as outfile:
-	reader = csv.reader(infile, quotechar='"', quoting=csv.QUOTE_ALL)
-	writer = csv.writer(outfile, quotechar='"', quoting=csv.QUOTE_ALL)
-	for row in reader:
-		writer.writerow(row)
 
 print("end")
